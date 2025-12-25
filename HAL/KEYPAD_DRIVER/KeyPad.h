d160f819556158aaba4329a7685c5304743d1a7e
Belal Hani Sabha <137720072+belalsabha@users.noreply.github.com>
2025-12-26 00:24:42 +0200

    Add HAL drivers (KeyPad, LCD, Seven Segment)
    This commit adds HAL (Hardware Abstraction Layer) drivers for STM32F103C6:
    
    - KeyPad driver: supports matrix keypad scanning and reading
    - LCD driver: character LCD (16x2) initialization, writing, clearing
    - Seven Segment driver: control of 7-segment displays
    
    These drivers use the underlying MCAL drivers (GPIO, RCC, etc.) for hardware access.

---
+++ HAL/KEYPAD_DRIVER/KeyPad.h
@@ -0,0 +1,32 @@
+/*
+ * keyPad.h
+ *
+ *  Created on: Dec 10, 2025
+ *  Author: Belal Hani Sabha
+ */
+
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
+
+#include "GPIO.h"
+#include "Stm32f103x6.h"
+
+
+#define KEYPAD_PORT GPIOB
+
+// Rows
+#define R0 GPIO_PIN0
+#define R1 GPIO_PIN1
+#define R2 GPIO_PIN3
+#define R3 GPIO_PIN4
+
+// Columns
+#define C0 GPIO_PIN5
+#define C1 GPIO_PIN6
+#define C2 GPIO_PIN7
+#define C3 GPIO_PIN8
+
+void    Keypad_Init();
+uint8_t GetData_FromKeyPad();
+
+#endif /* KEYPAD_H_ */
